f5b213f9e0e0650ea3d26ab9fdb047011ca15345
pchmi-t <vincitquisevincit@abv.bg>
2016-06-23 05:18:37 +0300

    login fix

--- htmls/auth.js
+++ htmls/auth.js
@@ -1,16 +1,36 @@
 $(document).ready(function() {
-	$("#logoutbtn").on("click", function(event) {
-		event.preventDefault();
-		localStorage.removeItem("user");
-		window.location.href= "Home.html";
-	});
+	$("#signin").on("click", login);
+	$("#logoutbtn").on("click", logout);
 
-	if (localStorage.user  == null || localStorage.user  == "") {
-		console.log("baba");
-		$(".unauth").show();
-		$(".logOut").hide();
+	var currentUser = sessionStorage.getItem('user');
+	if (!currentUser) {
+		console.log("not logged in");
+		$("#loginbtn").show();
+		$("#regbtn").show();
+		$("#profilebtn").hide();
+		$("#logoutbtn").hide();
 	} else {
-		$(".unauth").hide();
-		$(".logOut").show();
+		console.log("logged in as " + currentUser);
+		$("#loginbtn").hide();
+		$("#regbtn").hide();
+		$("#profilebtn").show();
+		$("#logoutbtn").show();
 	}
 });
+
+function login() {
+	var userEmail = $("#useremail").val();
+	if (userEmail == null || userEmail == "") {
+		$("#errormsg").show();
+		$("#errormsg").slideToggle(3000);
+	} else {
+		sessionStorage.setItem("user", userEmail);
+		window.location.href= "profile.html";
+	}
+}
+
+function logout() {
+	event.preventDefault();
+	sessionStorage.removeItem("user");
+	window.location.href= "Home.html";
+}
