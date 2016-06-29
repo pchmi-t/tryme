afb6d7d0378d3b1f02aec486ba9a395948af1d48
EmineBasheva <eminka23@abv.bg>
2016-06-29 21:35:10 +0300

    home page ui is ready

--- WebContent/js/navbar.js
+++ WebContent/js/navbar.js
@@ -5,11 +5,11 @@ $(document).ready(function() {
       //.log to determine when you want the
       //nav bar to stick.
       console.log($(window).scrollTop())
-    if ($(window).scrollTop() < 201) {
+    if ($(window).scrollTop() < 81) {
       $('.upper').slideDown("fast");
       $('.navbar').attr("style", "top: 0 !important");
     }
-    if ($(window).scrollTop() > 200) {
+    if ($(window).scrollTop() > 80) {
       $('.upper').slideUp("fast");
       $('.navbar').attr("style", "top:" + $(window).scrollTop() + " !important");
     }
